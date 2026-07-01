dfe899016d394ac845d85975d40f70b555861ec2
cfaet <praetoriam.fabricus@disroot.org>
2026-07-01 15:32:52 -0500

    Rework for article og metadata

--- src/Mooneghan/Contexts.hs
+++ src/Mooneghan/Contexts.hs
@@ -10,10 +10,21 @@ module Mooneghan.Contexts (
 ) where
 
 import Data.List (isSuffixOf)
+import Data.Char (isSpace)
+import qualified Data.List as List
 import Hakyll
 
 siteRoot :: String
 siteRoot = "https://blog.cfmoon.net"
+
+siteTitle :: String
+siteTitle = "The Aleph Project"
+
+siteDescription :: String
+siteDescription = "A blog by CFMoon | Musings on software and life."
+
+fallbackMetaImage :: String
+fallbackMetaImage = siteRoot ++ "/static/images/og-image-v2.png"
 
 cleanIndexUrl :: String -> String
 cleanIndexUrl url
@@ -29,10 +40,71 @@ cleanUrlField key = field key $ \item -> do
         Nothing -> noResult $ "No route found for " ++ show (itemIdentifier item)
         Just path -> return . cleanIndexUrl $ toUrl path
 
+stripWhitespace :: String -> String
+stripWhitespace = dropWhileEnd isSpace . dropWhile isSpace
+  where
+    dropWhileEnd predicate = reverse . dropWhile predicate . reverse
+
+metadataValue :: Identifier -> [String] -> Compiler (Maybe String)
+metadataValue _ [] = return Nothing
+metadataValue ident (key : keys) = do
+    value <- getMetadataField ident key
+    case stripWhitespace <$> value of
+        Just x | not (null x) -> return $ Just x
+        _ -> metadataValue ident keys
+
+absoluteUrl :: String -> String
+absoluteUrl rawPath
+    | "https://" `List.isPrefixOf` path = path
+    | "http://" `List.isPrefixOf` path = path
+    | "/" `List.isPrefixOf` path = siteRoot ++ path
+    | otherwise = siteRoot ++ "/" ++ dropRelativePrefix path
+  where
+    path = stripWhitespace rawPath
+
+    dropRelativePrefix p
+        | "../" `List.isPrefixOf` p = dropRelativePrefix $ drop 3 p
+        | "./" `List.isPrefixOf` p = dropRelativePrefix $ drop 2 p
+        | otherwise = p
+
+siteMetaCtx :: Context String
+siteMetaCtx =
+    constField "metaTitle" siteTitle
+        <> constField "metaDescription" siteDescription
+        <> constField "metaImage" fallbackMetaImage
+        <> constField "metaImageAlt" siteDescription
+        <> constField "metaType" "website"
+
+postMetaCtx :: Context String
+postMetaCtx =
+    constField "metaType" "article"
+        <> constField "isArticle" "true"
+        <> field "metaTitle" postTitle
+        <> field "metaDescription" postDescription
+        <> field "metaImage" postImage
+        <> field "metaImageAlt" postImageAlt
+  where
+    postTitle item = do
+        title <- metadataValue (itemIdentifier item) ["title"]
+        return $ maybe siteTitle (\x -> siteTitle ++ " - " ++ x) title
+
+    postDescription item =
+        metadataValue (itemIdentifier item) ["description", "subtitle"]
+            >>= return . maybe siteDescription id
+
+    postImage item =
+        metadataValue (itemIdentifier item) ["image"]
+            >>= return . maybe fallbackMetaImage absoluteUrl
+
+    postImageAlt item =
+        metadataValue (itemIdentifier item) ["imageAlt", "subtitle", "title"]
+            >>= return . maybe siteDescription id
+
 mooneghanCtx :: Context String
 mooneghanCtx =
     constField "family" "Catrin ♥ Fabrizio | Eleonora"
         <> constField "siteRoot" siteRoot
+        <> siteMetaCtx
         <> cleanUrlField "cleanUrl"
         <> constField "bound" "Eternally Connected Across Time and Space"
         <> constField "creators" "Catrin & Fabrizio"
@@ -41,12 +113,15 @@ mooneghanCtx =
         <> dateField "dateIso" "%Y-%m-%d"
         <> modificationTimeField "updated" "%B %e, %Y"
         <> modificationTimeField "updatedIso" "%Y-%m-%dT%H:%M:%SZ"
-        <> constField "mDescription" "A blog by CFMoon | Musings on software and life."
-        <> constField "mTitle" "The Aleph Project"
+        <> constField "mDescription" siteDescription
+        <> constField "mTitle" siteTitle
         <> defaultContext
 
 postCtx :: Context String
-postCtx = mooneghanCtx <> dateField "atomDate" "%Y-%m-%dT%H:%M:%SZ"
+postCtx =
+    postMetaCtx
+        <> mooneghanCtx
+        <> dateField "atomDate" "%Y-%m-%dT%H:%M:%SZ"
 
 indexCtx :: Context String
 indexCtx =
@@ -62,6 +137,7 @@ postsCountField x = field "postCount" $ \_ -> do
 archiveCtx :: Context String
 archiveCtx =
     constField "title" "The Archives"
+        <> constField "metaTitle" (siteTitle ++ " - The Archives")
         <> constField "post-list-heading" "All Posts"
         <> postsCountField "posts/*"
         <> mooneghanCtx
